eff9053677f3a0f1418c80a66522080fa75b09dc
Brendan McGinty <brendan.mcginty14@gmail.com>
2024-01-18 20:57:21 -0500

    added initial API URL Links to JS

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,7 +1,24 @@
-const weatherAPI = 'https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=7b1eab3296911715f248b7a79b72ba34'
+let weatherAPI = 'https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=7b1eab3296911715f248b7a79b72ba34'
+let geoAPI = `https://api.opencagedata.com/geocode/v1/json?q=${city-input}%2C+${country-input}&key=b378f52f4b0d4e9cb5a38bbee769e7f5`
+let searchButton = document.getElementById('search-button')
+
+fetch(geoAPI)
+.then(response => response.json())
+.then(data => {
+    console.log(data);
+})
 
 fetch(weatherAPI)
 .then(response => response.json())
 .then(data => {
   console.log(data);
 });
+
+searchButton.addEventListener('click', search => {
+    search.preventDefault();
+
+    const cityName = document.getElementById('city-input');
+
+
+
+})
